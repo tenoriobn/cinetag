f511542dc29a11e734efb9970fa95c2922701232
Bruno Tenório <tenoriobn@gmail.com>
2023-08-15 16:26:18 -0300

    Criando cards dinâmicamente

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,9 +1,12 @@
-import Banner from 'components/Banner'
-import Cabecalho from 'components/Cabecalho'
-import Card from 'components/Card'
-import Rodape from 'components/Rodape'
-import Titulo from 'components/Titulo'
-import React from 'react'
+import Banner from 'components/Banner';
+import Cabecalho from 'components/Cabecalho';
+import Card from 'components/Card';
+import Rodape from 'components/Rodape';
+import Titulo from 'components/Titulo';
+import React from 'react';
+import videos from 'json/db.json';
+
+import styles from './Inicio.module.css';
 
 export default function Inicio() {
     return (
@@ -14,7 +17,11 @@ export default function Inicio() {
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
 
-            <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format-src&type=png' />
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Rodape />
         </>
     )
