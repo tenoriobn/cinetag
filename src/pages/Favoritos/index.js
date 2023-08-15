5e29d794e004e5c276261b778c16c590d2be8048
Bruno Tenório <tenoriobn@gmail.com>
2023-08-15 16:47:51 -0300

    Importando componentes na página Favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './Favoritos.module.css'
+import Banner from 'components/Banner';
+import Titulo from 'components/Titulo';
+import Card from 'components/Card';
 
 export default function Favoritos() {
     return (
         <>
-            
+            <Banner imagem="favoritos" />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+
+            <section className={styles.container}>
+                <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
+            </section>
         </>
     )
 }
