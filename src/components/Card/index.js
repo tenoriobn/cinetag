59a4ef38c215879d3cbc2f8767170d3d30a88b38
Bruno Tenório <tenoriobn@gmail.com>
2023-08-16 10:40:05 -0300

    Implementando o hook de favoritar no Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import styles from './Card.module.css';
 import iconeFavoritar from './favoritar.png';
+import iconeDesfavoritar from './desfavoritar.png';
+import { useFavoritoContext } from 'context/Favoritos';
 
 export default function Card({ id, titulo, capa }) {
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = ehFavorito ? iconeDesfavoritar : iconeFavoritar;
+
     return (
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar} alt="Favoritar filmes" className={styles.favoritar} />
+            <img 
+                src={icone} 
+                alt="Favoritar filmes" 
+                className={styles.favoritar} 
+                onClick={() => {
+                    adicionarFavorito({id, titulo, capa})
+                }}
+            />
         </div>
     )
 }
