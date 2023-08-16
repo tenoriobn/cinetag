2841caccc490748a329a32eafe645725ba771d59
Bruno Tenório <tenoriobn@gmail.com>
2023-08-16 10:49:16 -0300

    Exibindo lista de favoritos na pagina Favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -3,8 +3,11 @@ import styles from './Favoritos.module.css'
 import Banner from 'components/Banner';
 import Titulo from 'components/Titulo';
 import Card from 'components/Card';
+import { useFavoritoContext } from 'context/Favoritos';
 
 export default function Favoritos() {
+    const { favorito } = useFavoritoContext();
+
     return (
         <>
             <Banner imagem="favoritos" />
@@ -13,7 +16,9 @@ export default function Favoritos() {
             </Titulo>
 
             <section className={styles.container}>
-                <Card id='1' titulo='Gato bonifácio' capa='https://thecatapi.com/api/images/get?format=src&type=png' />
+                {favorito.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
             </section>
         </>
     )
